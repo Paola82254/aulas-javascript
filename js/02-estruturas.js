c1edc0a36b649ce0a3b53db5d25f6149c9d232e8
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-05 16:36:51 -0300

    Update 02-estruturas.js

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -5,8 +5,15 @@
 
 // Objetos
 // Lista de dados não indexados e formados por propriedades e valores.
-// Arrays
+// Arrays (os índices sempre começam no zero)
 let cursos = ["Node.js", "React", "SQL", "UX/UI Design"]
 
 console.log(cursos); // Saída direta/estruturada
 console.log(cursos[2]); // SQL 
+
+// 1. Crie uma constante chamada "dados" e coloque dentro dela os nomes de uma pessoa (nome, sobrenome, idade, telefone, cidade)
+
+const dados = ["Mateus", "Pires", 17, "(11)99999-9999", "São Paulo"]
+
+// Mostre no console uma mensagem contendo somente o nome e a idade da pessoa. Exemplo: "Fulano tem 25 anos"
+console.log(`${dados[0]} tem ${dados[2]} anos.`)
