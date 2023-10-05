e25de8bdc43e4b4cc77b5390ed3d6f0a115bfc11
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-05 15:03:39 -0300

    comecodojs

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aulas-javascript
  Atividades com Linguagem JavaScript
+## Sobre a linguagem
+
+ - Javascript NÃO É Java
+ - JavaScript pode ser abreviado como JS
+ - É uma Linguagem voltada principalmente para o Front-End mas também pode ser usado para Back-End através do Node.js
+ - JS pode ser usado para criar recursos interativos nos sites, como por exemplo:
+   - Galeria de fotos
+   - Carroussel/Slider de conteúdo
+   - Funcionalidades de navegação
+   - Validações mais avançadas de formulário
+ - JS pode ser usado até mesmo para criar aplicativos e games.
---
+++ js/01-sintaxe.js
@@ -0,0 +1,24 @@
+console.log("Programador Web")
+// 
+/* Manipulando dados */
+
+// Variavéis
+let curso = "HTML5 e CSS3"
+let ano = 2023
+
+// Constantes
+const mediaMinima = 7
+
+// Saída de dados a partir da memória
+console.log(curso)
+console.log(mediaMinima)
+console.log(ano);
+
+// Saída de dados personalizada
+
+// Estou fazendo curso de HTML5 e CSS3 neste ano de 2023
+// Template String/Literal: Saída usando crases e ${}
+console.log(`Estou fazendo o curso de ${curso}.`);
+
+//Concatenação (forma mais antiga de saída, mas ainda válida)
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano+".")
