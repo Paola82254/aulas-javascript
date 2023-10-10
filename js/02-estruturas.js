c94facf9943ad5ebae2f04f4fa8a5c92e075bec6
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-10 14:40:07 -0300

    Update 02-estruturas.js

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -48,3 +48,54 @@ let dadosPessoais = {
 // 2)Mostra uma mensagem com pelo menos 2 propriedades do objeto que você criou
 
 console.log(`Meu filme favorito é ${dadosPessoais.filme} e minha comida favorita é ${dadosPessoais.comida}`);
+
+console.log('------');
+
+// Objeto com array e com outro objeto
+
+let pessoa = {
+    nome: "Shiryu",
+    idade: 20,
+    telefones: ["11-2135-0300", "11-91234-5678"],
+    medidas: {
+        peso: 65,
+        altura: 1.75,
+    }
+};
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`Celular: ${pessoa.telefones[1]}`);
+console.log(`Peso: ${pessoa.medidas.peso} kg`);
+
+// Array de objetos
+
+let alunos = [
+    {
+        nome: "Naruto",
+        idade: 10
+    },
+    {
+        nome: "Guts",
+        idade: 30
+    },
+    {
+        nome: "Dohko",
+        idade: 120
+    }
+];
+
+console.log( alunos[1].nome); // Guts
+console.log( alunos[0].nome); // Naruto
+console.log(`O cavaleiro de ouro originalmente era o ${alunos[2].nome} de Libra que tem ${alunos[2].idade} anos`);
+
+// Array como matriz
+
+let tecnologias = [
+    ["HTML5", "CSS3", "JavaScript"],
+    ["PHP", "SQL", "APIs", "Node", "Python"],
+    ["Figma", "Photoshop"]
+];
+
+console.log(tecnologias[1][3]); // Node
+console.log(tecnologias[2][1]); // Photoshop
+console.log(tecnologias[0][0]); // HTML5
