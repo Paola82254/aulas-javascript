c9789f550830e9cfbd4e50419ea5969c6ac753c1
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-11 14:17:53 -0300

    11-10-update

---
+++ js/operadoreslogicos.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Requisitos da escola para aprovação
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+// Resultados do aluno fictício
+let aluno = "Jean"
+let mediaFinal = 8.5;
+let faltas = 8;
+let resultado
+
+//Operador Lógico E (AND)
+
+if(mediaFinal >= mediaMinima && < limiteDeFaltas){
+    resultado = "Aprovado;"
+} else {
+    resultado = "Reprovado";
+}
+
+console.log(`O aluno ${aluno} está ${resultado}`);
+console.log(`Média do aluno: ${mediaFinal}`);
+console.log(`Faltas do Aluno: ${faltas}`);
