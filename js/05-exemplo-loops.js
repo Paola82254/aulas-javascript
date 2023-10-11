98fd82ff17d81312a8398860d314bc314c3c39d4
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-11 16:20:25 -0300

    Update 05-exemplo-loops.js

--- js/05-exemplo-loops.js
+++ js/05-exemplo-loops.js
@@ -9,9 +9,18 @@ Instruções para realizar ações por uma quantidade específica de vezes. */
 
 //  OBS: Normalmente o loop é controlado através de uma variável contadora
 
-let contador = 1;
+//Exemplo WHILE (ENQUANTO)
+/* let contador = 1;
 
 while( contador<= 5 ){
     console.log(`Valor de contador: ${contador}`);
+    contador++; //++incremento
+} */
+
+//EXEMPLO DO/WHILE
+let contador = 10;
+
+do {
+    console.log(`Contador vale: ${contador}`);
     contador++;
-}
+} while( contador <= 3 )
