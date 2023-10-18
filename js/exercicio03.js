9b731e0de9bdbd202136ad41ccbc631e94f6d1fd
Paola82254 <teacherpaola4kids@gmail.com>
2023-10-18 14:57:06 -0300

    Update exercicio03.js

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -15,19 +15,35 @@ Dica: para converter, basta pegar o valor e multiplicar pela cotação do dólar
 
 4. Após a conversão, mostre o valor original em dólar e o valor convertido para reais. */
 
-let valor= 1000;
-let dolar = 5.3;
+"use strict"
 
-function converteValor(valor){
-    let opcoes = {
-        style: "currency",
-        currency: "BRL"
-    return valor * dolar;
+//dados de entrada (exemplos de valores)
+
+let valorDaCotacaoDoDia = 5.03;
+let valorEmDolares = 2500;
+
+//função que realiza a conversão matemática ao receber PARÂMETROS
+function converteMoeda (valor, cotacao) {
+    return valor * cotacao
 }
 
-    console.log(converteValor(valor))
+//variável que receberá o resultado processado pela função DEPOIS que a função terminar sua execução
+
+let valorConvertido = converteMoeda(valorDaCotacaoDoDia, valorEmDolares)
+
+//saídas após o processamento
+
+console.log(valorEmDolares); // quanto que era em dólares
+console.log(valorConvertido);  // quanto que fica em reais
+
+console.log(formatarMoeda(valorEmDolares, "en", "USD"));
+console.log(formatarMoeda(valorConvertido, "pt-br", "BRL"));
 
 
+function formatarMoeda(valor, local, moeda){
+    let opcoes = {
+        style: "currency",
+        currency: moeda
     }
-return valor.toLocaleString("pt-br",opcoes)
+return valor.toLocaleString(local,opcoes)
 }
